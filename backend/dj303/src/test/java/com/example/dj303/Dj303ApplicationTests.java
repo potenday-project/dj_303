b9f20be5e0b8116a512cb102ac042bb33fa70928
yht0827 <yangheetae0827@naver.com>
2023-03-27 00:18:25 +0900

    init commit

---
+++ backend/dj303/src/test/java/com/example/dj303/Dj303ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dj303;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dj303ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
