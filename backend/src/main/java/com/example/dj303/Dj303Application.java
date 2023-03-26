bce1c245339bbedb606158fe6db04d9d293acee2
yht0827 <yangheetae0827@naver.com>
2023-03-27 00:22:02 +0900

    init commit

---
+++ backend/src/main/java/com/example/dj303/Dj303Application.java
@@ -0,0 +1,13 @@
+package com.example.dj303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dj303Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dj303Application.class, args);
+	}
+
+}
